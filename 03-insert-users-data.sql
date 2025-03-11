3e92c61c4e463ce5f5941414a841b8f59ee23c68
Anish Shukla <shuklaanish040@gmail.com>
2025-03-11 19:25:41 +0530

    sql

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (yearly_salary, full_name, current_status) VALUES(0,'Anish SHukla','unemployed');
+
+INSERT INTO users(yearly_salary,full_name,current_status) VALUES(20000,'Ravi Shukla','employed');
+
+INSERT INTO users(yearly_salary, full_name,current_status) VALUES (50000,'Dinesh Shukla','self-employed');
