0984312f390495a9963f07a64be4fd7c5f0492c8
Anish Shukla <shuklaanish040@gmail.com>
2025-03-11 23:32:43 +0530

    sql

--- 05-create-employers-table.sql
+++ 05-create-employers-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE employers(
   company_name VARCHAR(250),
   company_address VARCHAR(300),
-/*yearly_revenue FLOAT(5,2) -- Approximation, Allowed: 123.22, 12.1 , not Allowed: 123.222 */
+/*yearly_revenue FLOAT(5,2) -- Approximation, Allowed: 123.22, 12.1 , not   Allowed: 123.222 */
   yearly_revenue NUMERIC(5,2), /*Exact value, Allowed: 123.12 ,not Allowed:123123*/
   is_hiring BOOLEAN
 );
