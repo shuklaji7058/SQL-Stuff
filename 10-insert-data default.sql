0c91bd13d4b0a2b15b2fba3f07fe7b9385867b41
Anish Shukla <shuklaanish040@gmail.com>
2025-03-12 19:38:58 +0530

    sql

---
+++ 10-insert-data default.sql
@@ -0,0 +1,11 @@
+INSERT INTO employers (company_name, company_address, yearly_revenue)
+ VALUES('Tata','Mumbai', 200.21);
+
+INSERT INTO employers (company_name, company_address, yearly_revenue)
+ VALUES('Tech-Mahindra','Pune', 26.21);
+
+INSERT INTO employers (company_name, company_address, yearly_revenue)
+ VALUES('Capgemini','Banglore', 20.21);
+
+INSERT INTO conversations(user_name, employer_name, message)
+ VALUES('Anish Shukla','Tata','Hello, I am interested in the job');
