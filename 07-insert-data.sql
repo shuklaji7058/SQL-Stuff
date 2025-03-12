8da95ea3ad6194139638d05a98fa00daa4e69cca
Anish Shukla <shuklaanish040@gmail.com>
2025-03-12 12:29:23 +0530

    sql

--- 07-insert-data.sql
+++ 07-insert-data.sql
@@ -6,3 +6,6 @@
 
 -- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
 --  VALUES('Capgemini','Banglore', 20.21, TRUE);
+
+INSERT INTO conversations(user_name, employer_name, message, date_sent)
+ VALUES('Anish SHukla','Tata','Hello, I am interested in the job', '2020-01-01 13:23:21');
