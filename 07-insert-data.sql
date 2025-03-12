60ec907d37ce22b02b8acab18c4bca2deadd1190
Anish Shukla <shuklaanish040@gmail.com>
2025-03-12 12:19:58 +0530

    sql

---
+++ 07-insert-data.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--  VALUES('Tata','Mumbai', 200.21, TRUE);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--  VALUES('Tech-Mahindra','Pune', 26.21, FALSE);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring)
+--  VALUES('Capgemini','Banglore', 20.21, TRUE);
