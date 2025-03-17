eac53d7e5b6e8446d1fb16ff5cc95598c30f8fc2
Anish Shukla <shuklaanish040@gmail.com>
2025-03-17 12:39:26 +0530

    sql

---
+++ 20-exercise.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE online_shop;
+
+CREATE TABLE products(
+  product_name VARCHAR(300) NOT NULL,
+  price NUMERIC(10,2) NOT NULL CHECK (price > 0),
+  description TEXT NOT NULL,
+  amount_in_stock SMALLINT,
+  image_url VARCHAR(300),
+);
+
+INSERT INTO products('producct_name','price','description','amount_in_stock','image_url')
+VALUES ('Laptop', 1000, 'A laptop for all your needs', 10, 'https://example.com/laptop.jpg'),
+       ('Mouse', 20, 'A mouse for all your needs', 100, 'https://example.com/mouse.jpg');
+
+ALTER TABLE products
+ADD COLUMN id INT PRIMARY KEY AUTO_INCREMENT;
