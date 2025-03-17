f27ec2d0c4ea49e9f22173ef1af806ad9dac7d83
Anish Shukla <shuklaanish040@gmail.com>
2025-03-17 20:18:31 +0530

    sql

--- 20-exercise.sql
+++ 20-exercise.sql
@@ -5,12 +5,13 @@ CREATE TABLE products(
   price NUMERIC(10,2) NOT NULL CHECK (price > 0),
   description TEXT NOT NULL,
   amount_in_stock SMALLINT,
-  image_url VARCHAR(300),
+  image_url VARCHAR(300)
 );
+INSERT INTO products(product_name, price, description, amount_in_stock,image_url)
+VALUES ('Laptop', 1000, 'A laptop for all your needs', 10, 'https://example.com laptop.jpg');
 
-INSERT INTO products('producct_name','price','description','amount_in_stock','image_url')
-VALUES ('Laptop', 1000, 'A laptop for all your needs', 10, 'https://example.com/laptop.jpg'),
-       ('Mouse', 20, 'A mouse for all your needs', 100, 'https://example.com/mouse.jpg');
+INSERT INTO products(product_name, price, description, amount_in_stock,image_url)
+VALUES('Mouse', 20, 'A mouse for all your needs', 100, 'https://example.com/mouse.jpg');
 
 ALTER TABLE products
 ADD COLUMN id INT PRIMARY KEY AUTO_INCREMENT;
