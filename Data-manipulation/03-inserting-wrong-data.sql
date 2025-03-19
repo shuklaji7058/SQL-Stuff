30a15029039a558a70cf5f099c16ab4bb177fb28
Anish Shukla <shuklaanish040@gmail.com>
2025-03-19 20:16:50 +0530

    sql

---
+++ Data-manipulation/03-inserting-wrong-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO sales(
+  date_created,
+  date_fulfilled,
+  customer_name,
+  product_name,
+  volume
+)
+VALUES (
+  '2023-01-30',
+  '2023-02-01',
+  'Grace Lee',
+  'Poduct I',
+  100.0
+);
---
+++ Data-manipulation/04-update-data.sql
@@ -0,0 +1,5 @@
+UPDATE sales
+SET
+  product_name = 'Product I',
+  volume = volume * 1000
+WHERE id = 11;
