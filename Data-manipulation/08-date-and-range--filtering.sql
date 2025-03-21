0ed582cc48b9f08320505fb698fca86fd671e79d
Anish Shukla <shuklaanish040@gmail.com>
2025-03-21 11:41:18 +0530

    sql

---
+++ Data-manipulation/08-date-and-range--filtering.sql
@@ -0,0 +1,8 @@
+-- SELECT * FROM sales
+-- WHERE product_name BETWEEN 'Product B' AND  'Product G';
+
+-- SELECT * FROM sales
+-- WHERE volume BETWEEN 100 AND 300;
+
+SELECT * FROM sales
+WHERE is_disputed < FALSE AND TRUE;
