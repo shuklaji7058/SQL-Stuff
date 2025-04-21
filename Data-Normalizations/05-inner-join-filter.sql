62f9385438606fc8e9f1bbf1c095fdba73d61c53
Anish Shukla <shuklaanish040@gmail.com>
2025-04-21 20:42:26 +0530

    sql

--- Data-Normalizations/05-inner-join-filter.sql
+++ Data-Normalizations/05-inner-join-filter.sql
@@ -1,4 +1,6 @@
 SELECT u.id, u.first_name, u.last_name, u.email, a.street, a.house_number, c.name AS city_name
 FROM users AS u
 INNER JOIN addresses AS a ON u.address_id = a.id
-INNER JOIN cities AS c ON a.city_id = c.id;
+INNER JOIN cities AS c ON a.city_id = c.id
+WHERE c.id =1 OR c.id = 2
+ORDER BY u.id DESC;
