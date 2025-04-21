abcd40244ed29c7e23f3d30971606979eb1fae4b
Anish Shukla <shuklaanish040@gmail.com>
2025-04-21 12:56:54 +0530

    sql

---
+++ Data-Normalizations/03-insert-data.sql
@@ -0,0 +1,37 @@
+-- INSERT INTO cities(name)
+-- VALUES('Delhi'),
+-- ('Mumbai'),
+-- ('Bangalore'),
+-- ('Chennai'),
+-- ('Kolkata'),
+-- ('Hyderabad'),
+-- ('Ahmedabad'),
+-- ('Pune'),
+-- ('Jaipur'),
+-- ('Lucknow');
+
+-- INSERT INTO addresses(street, house_number, city_id)
+-- VALUES('DG Road', '12A', 1),
+-- ('MG Road', '12/1', 2),
+-- ('BG Road', '12/2', 3),
+-- ('CG Road', '12/3', 4),
+-- ('KG Road', '12/4', 5),
+-- ('HG Road', '12/5', 6),
+-- ('AG Road', '12/6', 7),
+-- ('PG Road', '12/7', 8),
+-- ('JG Road', '12/8', 9),
+-- ('LG Road', '12A', 10),
+-- ('DG Road', '12B', 1);
+
+INSERT INTO users(first_name, last_name, email, address_id) 
+VALUES
+('Anish','shukla','anish@test.com',1),
+('Manish','shukla','manish@test.com',2),
+('Ravi','shukla','ravi@test.com',3),
+('Ritu','shukla','ritu@test.com',4),
+('Rinku','shukla','rinku@test.com',5),
+('Maya','shukla','maya@test.com',6),
+('Dinesh','shukla','dinesh@test.com',7),
+('Aarush','shukla','aarush@test.com',8),
+('Parth','shukla','parth@test.com',9),
+('Gaurav','shukla','gaurav@test.com',10);
