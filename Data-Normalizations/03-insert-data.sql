77fa1dbb9879b1057e39426e6601f65038536522
Anish Shukla <shuklaanish040@gmail.com>
2025-04-21 20:26:31 +0530

    sql

--- Data-Normalizations/03-insert-data.sql
+++ Data-Normalizations/03-insert-data.sql
@@ -1,18 +1,19 @@
--- INSERT INTO cities(name)
+-- INSERT INTO cities(id,name)
 -- VALUES
--- ('Delhi'),
--- ('Mumbai'),
--- ('Bangalore');
+-- (1,'Delhi'),
+-- (2,'Mumbai'),
+-- (3,'Bangalore');
 
--- INSERT INTO addresses(street, house_number, city_id)
+-- INSERT INTO addresses(id,street, house_number, city_id)
 -- VALUES
--- ('DG Road', '12A', 3),
--- ('MG Road', '12/1', 2),
--- ('BG Road', '12/2', 1),
--- ('TG ROAD', '12/3', 4);
+-- (1,'DG Road', '12A', 1),
+-- (2,'MG Road', '12/1', 2),
+-- (3,'BG Road', '12/2',3);
 
-INSERT INTO users(first_name, last_name, email, address_id) 
-VALUES
-('Anish','shukla','anish@test.com',3),
-('Manish','shukla','manish@test.com',2),
-('Ravi','shukla','ravi@test.com',4);
+-- INSERT INTO users(id,first_name, last_name, email, address_id) 
+-- VALUES
+-- (1,'Anish','shukla','anish@test.com',1),
+-- (2,'Manish','shukla','manish@test.com',2),
+-- (3,'Ravi','shukla','ravi@test.com',3);
+
+
--- Data-Normalizations/04-inner-join.sql
+++ Data-Normalizations/04-inner-join.sql
@@ -1,3 +1,4 @@
-SELECT u.id, u.first_name, u.last_name, u.email, a.street, a.house_number, city_id 
+SELECT u.id, u.first_name, u.last_name, u.email, a.street, a.house_number, c.name AS city_name
 FROM users AS u
-INNER JOIN addresses AS a ON u.address_id = a.id;
+INNER JOIN addresses AS a ON u.address_id = a.id
+INNER JOIN cities AS c ON a.city_id = c.id;
