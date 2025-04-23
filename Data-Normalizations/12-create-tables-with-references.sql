3ce10c18b33502ca60bffd3ecec98e07ace55f66
Anish Shukla <shuklaanish040@gmail.com>
2025-04-23 19:08:45 +0530

    sql

--- Data-Normalizations/12-create-tables-with-references.sql
+++ Data-Normalizations/12-create-tables-with-references.sql
@@ -17,6 +17,17 @@ address_id INT REFERENCES addresses(id) ON DELETE CASCADE
 
 );
 
+-- for mysql foreign key constraints
+CREATE TABLE users(
+id INT PRIMARY KEY AUTO_INCREMENT, -- MYSQL
+-- id SERIAL PRIMARY KEY, -- POSTGRESQL
+first_name VARCHAR(300) NOT NULL,
+last_name VARCHAR(300) NOT NULL,
+email VARCHAR(300) NOT NULL,
+address_id INT NOT NULL,
+FOREIGN KEY (address_id) REFERENCES addresses(id) ON DELETE CASCADE -- ye syntax mysql ke liye hai agar aisa nahi kiya toh foreign key constraint nahi lagega aur hame koi error nahi milega (mysql me foreign key constraint lagane ke liye hame pehle table create karna padega jisme foreign key hai aur phir us table me foreign key ka reference dena padega jo ki pehle se exist karta ho, agar nahi kiya toh foreign key constraint nahi lagega aur hame koi error nahi milega)
+);
+
 
 CREATE TABLE cities(
 -- id INT PRIMARY KEY AUTO_INCREMENT, -- MYSQL
