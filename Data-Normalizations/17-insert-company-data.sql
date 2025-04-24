67d89598f9c0a6d203691b0cfeed770d6ac531b7
Anish Shukla <shuklaanish040@gmail.com>
2025-04-24 12:14:16 +0530

    sql

---
+++ Data-Normalizations/17-insert-company-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees(first_name,last_name,birthdate,email)
+VALUES('Anish','Shukla','2004-07-21','anish21@wow.com');
+
+INSERT INTO intranet_accounts(email,password)
+VALUES('anish21@wow.com','aasdfgh121');
