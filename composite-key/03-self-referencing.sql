0d322140d16250bb85659edcc7b2a1782ffb1eca
Anish Shukla <shuklaanish040@gmail.com>
2025-05-06 19:03:39 +0530

    sql

---
+++ composite-key/03-self-referencing.sql
@@ -0,0 +1,6 @@
+CREATE TABLe employees(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  supervisor_id INT REFERENCES employees ON DELETE SET NULL
+);
---
+++ composite-key/04-insert-self-referencinng-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name,last_name,supervisor_id) 
+VALUES
+('Anish','Shukla',NULL),
+('Ravi','Shukla',1),
+('Ritu','Shukla',1);
