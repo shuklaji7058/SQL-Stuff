7c43d274fa6168c1855fcff3a75bf3f193708e32
Anish Shukla <shuklaanish040@gmail.com>
2025-05-10 11:30:12 +0530

    sql

---
+++ Grouping-&-Aggregate-function/002-max-min.sql
@@ -0,0 +1,5 @@
+-- SELECT MAX(amount_billed) AS max_bill, max(amount_tipped) AS max_tipp FROM bookings;
+
+-- SELECT MAX(num_seats),MIN(num_seats) FROM tables;
+
+SELECT MAX(num_guests),MIN(num_guests) FROM bookings;
