e0c49a4463eb0c5e9ec5fb0e80eb11a0a45f357d
Anish Shukla <shuklaanish040@gmail.com>
2025-05-12 20:22:19 +0530

    sql

--- Grouping-&-Aggregate-function/008-window-functions.sql
+++ Grouping-&-Aggregate-function/008-window-functions.sql
@@ -1,1 +1,11 @@
-SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER (PARTITION BY booking_date) FROM bookings;-- This query calculates the total amount tipped for each booking date using a window function. The SUM() function is used as a window function with the OVER() clause, which allows us to perform calculations across a set of rows related to the current row without collapsing the result set into a single row per group. The PARTITION BY clause specifies that the calculation should be done for each unique booking_date.
+-- 1)
+-- SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER (PARTITION BY booking_date) FROM bookings;
+
+-- 2)
+-- SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER (PARTITION BY booking_date) FROM bookings;
+-- This query calculates the total amount tipped for each booking date using a window function. The SUM() function is used as a window function with the OVER() clause, which allows us to perform calculations across a set of rows related to the current row without collapsing the result set into a single row per group. The PARTITION BY clause specifies that the calculation should be done for each unique booking_date.
+
+-- 3)
+SELECT booking_date, amount_tipped, RANK() 
+OVER (PARTITION BY booking_date ORDER BY amount_tipped DESC)
+FROM bookings;
