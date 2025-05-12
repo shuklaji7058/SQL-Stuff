e7f0c732b0206964c87fac15f421476634cd20de
Anish Shukla <shuklaanish040@gmail.com>
2025-05-12 12:06:47 +0530

    sql

--- Grouping-&-Aggregate-function/005-group-by.sql
+++ Grouping-&-Aggregate-function/005-group-by.sql
@@ -1,4 +1,7 @@
-SELECT booking_date, SUM(num_guests)  FROM bookings
-GROUP BY booking_date; -- This query groups the results by booking_date and calculates the total number of guests for each date.
+-- SELECT booking_date, SUM(num_guests)  FROM bookings
+-- GROUP BY booking_date; -- This query groups the results by booking_date and calculates the total number of guests for each date.
 -- agar hame ye dono select statement ko sath me chaiye to group by use krna padega
 
+SELECT p.name,b.booking_date, SUM(num_guests),ROUND(AVG(amount_tipped),2) FROM payment_methods AS p
+INNER JOIN bookings AS b ON p.id=b.payment_id
+GROUP BY p.name, b.booking_date;
